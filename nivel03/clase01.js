14559cfed9c02fd3a79b64ee659dca8b7a03796c
jhonnycave <luis.j.cueva@gmail.com>
2021-05-07 21:08:42 -0500

    ejemplo 1

--- nivel03/clase01.js
+++ nivel03/clase01.js
@@ -1,5 +1,38 @@
-let objeto = {
-    "propiedad": "datos"
+
+
+"strict mode"
+
+const validar = (msj) => {
+
+    let edad ;
+    try {
+        if(msj){
+            edad = prompt(msj)
+        }else{
+            edad = prompt("introducir tu edad")
+
+        }
+
+        edad = parseInt(edad)
+
+        if(isNaN(edad)){
+            throw 'intrudce una edad validad o numerica '
+        }
+
+        if(edad>18){
+            console.log('tu eres mayor de edad');
+        }else{
+            console.log('tu eres menor de edad');
+        }
+        
+        
+
+    } catch (error) {
+        validar(error)
+    }
+    
+
 }
 
-console.log(objetp);
+
+validar()

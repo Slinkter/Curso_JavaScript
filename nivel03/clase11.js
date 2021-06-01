9fe33f97ebbd139814948280f03a5c9e4193935e
jhonnycave <luis.j.cueva@gmail.com>
2021-06-01 11:59:57 -0500

    clase 11 fin

--- nivel03/clase11.js
+++ nivel03/clase11.js
@@ -1,5 +1,13 @@
-if (navigator.serviceWorker){
-    navigator.serviceWorker.register("sw.js")
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("sw.js");
 }
 
-navigator.serviceWorker.ready.then(res => )
+navigator.serviceWorker.ready.then((res) =>
+  res.active.postMessage("(Navigator)el mensaje sera enviado al sw ")
+);
+
+// para que el servidor responsa
+navigator.serviceWorker.addEventListener("message", (e) => {
+  console.log("(Navigator) hemos recibio un mensaje del sw y esto es : ");
+  console.log(e.data);
+});

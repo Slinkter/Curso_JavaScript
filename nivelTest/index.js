a818184859a6abd0785242678d71230ea4cc653d
jhonnycave <luis.j.cueva@gmail.com>
2022-03-24 11:42:30 -0500

    repaso clase 08

--- nivelTest/index.js
+++ nivelTest/index.js
@@ -1,26 +1,11 @@
-const container = document.querySelector('.container')
-const titulo = document.querySelector('.titulo')
-const p1 = document.querySelector('.p1')
-const p2 = document.querySelector('.p2')
+const container = document.querySelector(".container");
+const item = document.createElement("li");
+const text = document.createTextNode("esto es un texto");
+item.appendChild(text);
+container.appendChild(item);
 
-document.write('<br/>',container)
-document.write('<br/>',titulo)
-document.write('<br/>',p1)
-document.write('<br/>',p2)
-document.write('<br/>')
-
-
-container.style.background = "#C7FFED"
-
-titulo.style.color = "red";
-titulo.style.fontSize = "80px"
-
-p1.style.fontSize = "40px"
-p2.style.fontSize = "20px"
-
-p1.style.color ="blue"
-p2.style.color ="#F29F05"
-
-
-
-
+for (i = 0; i <= 5; i++) {
+  const element = document.createElement("li");
+  element.textContent = "hola";
+  container.appendChild(element);
+}

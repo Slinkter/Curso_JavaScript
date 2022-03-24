815313d43788c4a67ef6edb367959774ad48580b
jhonnycave <luis.j.cueva@gmail.com>
2022-03-24 09:58:34 -0500

    pctogit

--- nivel01/clase07.js
+++ nivel01/clase07.js
@@ -1,34 +1,31 @@
-// Metodos de seleccion elemtos
+//-----> Metodos de seleccion elemtos
 // .getElementById
 // .getElementByTagName("p")
 // .querySelector
 // .querySelectorAll
-
-
-
-//-----> modificar css 
+//-----> modificar css
 // query selector coger el valor de las clases
+
 const titulo = document.querySelector(".titulo");
+titulo.style.color = "blue";
+titulo.style.padding = "10px";
+titulo.style.background = "red";
 //titulo.setAttribute("hidden", "true");
 //titulo.removeAttribute("hidden");
-titulo.style.color = "blue"
-titulo.style.padding = "50px"
-titulo.style.background = "red"
 //
-titulo.classList.add("classmartes")
-let valor_titulo = titulo.classList.item(1)
-document.write(valor_titulo)
-let valor_contains = titulo.classList.contains("classmartes")
-document.write("<br>",valor_contains)
-
+titulo.classList.add("classmartes");
+//
+let valor_titulo = titulo.classList.item(1);
+let valor_contains = titulo.classList.contains("classmartes");
 let resultado = titulo.textContent;
-document.write("<br>",resultado)
+
+document.write("<br>", valor_titulo);
+document.write("<br>", valor_contains);
+document.write("<br>", resultado);
 
 const input_001 = document.querySelector(".input_001");
 const input_002 = document.querySelector(".input_002");
 const input_normal = document.querySelector(".input_003");
-input_normal.minLenght = 1
-input_normal.placeholder ="hola"
-input_normal.required = "true"
-
-
+input_normal.minLenght = 1;
+input_normal.placeholder = "hola";
+input_normal.required = "true";
